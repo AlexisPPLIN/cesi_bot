de50487aaee24cba5d5ba0ddc0e944c0f4c897bc
Ekode <ekode@protonmail.com>
2020-06-22 20:29:58 +0200

    Added connection to database

--- index.js
+++ index.js
@@ -1,12 +1,29 @@
 const Discord = require('discord.js');
 const config = require('./config.json');
-const client = new Discord.Client();
 const Sequelize = require('sequelize');
+const client = new Discord.Client();
 
+// Initialize database ORM
+const sequelize = new Sequelize(
+	config.db_name,
+	config.db_user,
+	config.db_pass,
+	{
+		host: config.db_host,
+		dialect: config.db_type
+	}
+)
 
+sequelize.authenticate()
+	.then(() => {
+		console.log('Database connected !');
 
-client.once('ready', () => {
-	console.log('Ready!');
-});
+		client.once('ready', () => {
+			console.log('Ready!');
+		});
 
-client.login(config.token);
+		client.login(config.token);
+	})
+	.catch(err => {
+		throw new Error('Unable to connect to database : '+err);
+	})
