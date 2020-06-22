ff7cc142f9ac1fa6b2ae30c8c24b6456c3a2083c
AlexisPPLIN <alexis.poupelin@protonmail.com>
2020-06-22 20:14:59 +0200

    Added database parameters in config

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const Discord = require('discord.js');
 const config = require('./config.json');
 const client = new Discord.Client();
+const Sequelize = require('sequelize');
+
 
 
 client.once('ready', () => {
