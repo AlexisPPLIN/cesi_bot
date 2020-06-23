6d4facc4dcb2e6150abb52caa796f522bf0aeca0
justinbah <bahier.justin@gmail.com>
2020-06-23 21:52:24 +0200

    ajout commande

---
+++ embed/embed_confirmation_presence_MP.js
@@ -0,0 +1,23 @@
+const embed_confirmation_presence_mp = {
+    embed: {
+      
+      author: {
+       name: "CESI Bot",
+        url: "https://discordapp.com",
+        icon_url: "https://i.insider.com/5e5d2aa1fee23d10847a28f8?width=750&format=jpeg&auto=webp"
+      },
+      title: "Confirmation déclaration présence",
+      url: "https://discordapp.com",
+     description: "13/06/2020 - Matin",
+     
+      color: 10071592,
+      timestamp: "2020-06-13T10:05:38.205Z",
+      fields: [
+        {
+          name: "Etudiant",
+          value: "Alexis POUPELIN"
+        }
+      ]
+    }
+  };
+  console.log("fichier inclu");
--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 const Discord = require('discord.js');
 const config = require('./config.json');
+
+
+
+var fs = require("fs")
+var vm = require('vm')
+
+
+
+vm.runInThisContext(fs.readFileSync("./embed/embed_confirmation_presence_MP.js"))
+vm.runInThisContext(fs.readFileSync("./embed/embed_presence_jour.js"))
+
 const client = new Discord.Client();
 
 client.once('ready', () => {
@@ -7,3 +18,69 @@ client.once('ready', () => {
 });
 
 client.login(config.token);
+
+
+client.on('message', message => {
+	var MessageMinuscule = message.content.toLowerCase()//afin de pas prendre en compte les majuscule ou minuscule 
+	console.log("test3");
+	if (message.author.id !== client.user.id)//pour ignorer les msg du bot
+	{
+		console.log("test2");
+		if (MessageMinuscule === '!help') {
+			//afficher la liste des comande dans un embed
+		}
+
+
+		if (MessageMinuscule === '!présent' || MessageMinuscule === '!présente' || MessageMinuscule === '!present' || MessageMinuscule === '!presente') {
+
+
+			//modification dans la bd avec l'heure et en fct de l'id
+
+
+
+			//supresion message
+			embed_confirmation_presence_mp.embed.fields[0].value = message.author.username;//modifie le nom /* mettre le nom de la base de donne en fct de l'id
+
+			message.delete();//supprime le msg
+			//envoi mp confirmation
+
+			message.author.send({ embed: embed_confirmation_presence_mp.embed });
+		}
+
+
+
+
+
+		if (MessageMinuscule === '!liste élève') {
+
+		}
+
+
+
+
+
+		if (MessageMinuscule.slice(0, 10) == '!présences') {//si message commence par !présences
+			var etudiant = ['Alexis', 'justin'];
+			var ListeEtudiantChaine = "";
+			for (let i = 0; i < etudiant.length; i++) {
+				ListeEtudiantChaine = ListeEtudiantChaine + "🎓" + etudiant[i] + "\n";
+			}
+
+			embed_presence_jour.embed.fields[0].value=ListeEtudiantChaine;
+			message.author.send({ embed: embed_presence_jour.embed });
+		}
+
+
+		if (MessageMinuscule.slice(0, 6) == '!cours') {//si message commence par !cours
+		}
+
+		if (MessageMinuscule.slice(0, 5) == '!link') {//si message commence par !link
+		}
+
+
+		if (MessageMinuscule.slice(0, 7) == '!delete') {//si message commence par !delete
+		}
+
+	}
+
+});
