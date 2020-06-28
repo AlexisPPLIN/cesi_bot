e3ac6c499a44d082b95dec919512f9a4491d9b07
ekode <ekode@protonmail.com>
2020-06-28 15:46:05 +0200

    Moved id_discord to BIGINT(20)

--- models/utilisateur.js
+++ models/utilisateur.js
@@ -3,7 +3,7 @@ module.exports = (sequelize, DataTypes) => {
   const Utilisateur = sequelize.define('Utilisateur', {
     prenom: DataTypes.STRING,
     nom: DataTypes.STRING,
-    id_discord: DataTypes.INTEGER
+    id_discord: DataTypes.BIGINT(20)
   }, {});
   Utilisateur.associate = function(models) {
     Utilisateur.belongsTo(models.Role);
