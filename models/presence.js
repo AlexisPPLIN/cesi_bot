b61d8ed13a36529442a5b00988b2ef2119a1ffcd
ekode <ekode@protonmail.com>
2020-06-28 16:43:01 +0200

    Added date_arrive to Présences

--- models/presence.js
+++ models/presence.js
@@ -1,6 +1,8 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
-  const Presence = sequelize.define('Presence', {}, {});
+  const Presence = sequelize.define('Presence', {
+    date_arrive: DataTypes.DATE,
+  }, {});
   Presence.associate = function(models) {
     Presence.belongsTo(models.Utilisateur);
     Presence.belongsTo(models.Periode);
