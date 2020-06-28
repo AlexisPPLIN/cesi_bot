31a1c6272e5bcdda9904254addac0ff9a771b3e2
ekode <ekode@protonmail.com>
2020-06-28 19:23:45 +0200

    Added mongodb to config

---
+++ classes/PeriodPlanner.js
@@ -0,0 +1,29 @@
+class PeriodPlanner{
+    constructor() {
+        this.model = "";
+
+    }
+
+
+
+    /**
+     * Envoie l'embed de début de période
+     */
+    sendStartPeriodEmbed(){
+
+    }
+
+    /**
+     * Envoie l'embed de fin de période
+     */
+    sendEndPeriodEmbed(){
+
+    }
+
+    /**
+     * Génère un embed pour résumer le planning
+     */
+    generatePlanningEmbed(){
+
+    }
+}
