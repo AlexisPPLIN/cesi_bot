c23c9910c65d7ea58727253ee2dc4ec7baa227a9
ekode <ekode@protonmail.com>
2020-07-20 18:29:15 +0200

    Finished !askpresence
    Added documentation
    Added redis config

--- classes/PresenceSupervisor.js
+++ classes/PresenceSupervisor.js
@@ -1,15 +1,20 @@
+const db = require('../models/index');
+const moment = require('moment');
+const Queue = require('bull');
+const env = require('../config.json');
+
+moment.locale('fr')
+let embedQueue = new Queue('embed', 'redis://'+env.redis_host+':'+env.redis_port);
+
 const ArgumentValidationError = require('../Exceptions/ArgumentValidationError')
 const EndBeforeStartError = require('../Exceptions/EndBeforeStartError')
 const TimeAlreadyPassedError = require('../Exceptions/TimeAlreadyPassedError')
-const moment = require('moment');
-moment.locale('fr')
 
-const db = require('../models/index');
-const env = require('../config.json');
-
-const Queue = require('bull');
-let embedQueue = new Queue('embed', 'redis://127.0.0.1:6379');
-
+/**
+ * Supervisor of the !askpresence command
+ * @type {PresenceSupervisor}
+ * @author DevEkode
+ */
 module.exports = class PresenceSupervisor{
     constructor(start_arg,end_arg) {
         let start_date = this.parseDate(start_arg);
@@ -20,12 +25,22 @@ module.exports = class PresenceSupervisor{
         }
     }
 
+    /**
+     * Validate format of an argument (HH:MM)
+     * @param arg
+     * @returns {boolean}
+     */
     validateArgumentFormat(arg){
         let regex = /^([0-1][0-9]|2[0-3]):([0-5][0-9])$/
         if(!regex.test(arg)) throw new ArgumentValidationError(arg);
         return true;
     }
 
+    /**
+     * Parse the string given (HH:MM) into a Date object
+     * @param string_time
+     * @returns {Date}
+     */
     parseDate(string_time){
         if(this.validateArgumentFormat(string_time)){
             let date = new Date();
@@ -36,6 +51,12 @@ module.exports = class PresenceSupervisor{
         }
     }
 
+    /**
+     * Validate if the start and end dates do not overlap or inverted
+     * @param start_date
+     * @param end_date
+     * @returns {boolean}
+     */
     validateStartEndDate(start_date,end_date){
         if(start_date >= end_date) {
             throw new EndBeforeStartError();
@@ -48,6 +69,10 @@ module.exports = class PresenceSupervisor{
         return true;
     }
 
+    /**
+     * Save the period of this object to the database (if it doesn't exists)
+     * @param callback
+     */
     registerPeriodToDatabase(callback){
         db.Periode.findOrCreate({where : {debut: this.start, fin: this.end},defaults: {pre_debut: new Date()}})
             .then(([periode,created]) =>{
@@ -56,15 +81,33 @@ module.exports = class PresenceSupervisor{
             })
     }
 
-    planStartEmbedSend(channel_id){
-        let cron = moment(this.start).format("s m k D M d")
+    /**
+     * Register the start and end embed into the queue to be displayed later
+     * @param channel_id
+     */
+    planEmbedSend(channel_id){
+        let cron_start = moment(this.start).format("s m k D M d")
+        let cron_end = moment(this.end).format("s m k D M d")
 
+        // Plan start embed
         embedQueue.add({
             channel_id : channel_id,
             embed: this.generateStartPeriodEmbed()
         },{
             repeat:{
-                cron: cron,
+                cron: cron_start,
+                tz: "Europe/Paris",
+                limit: 1
+            }
+        });
+
+        //Plan end embed
+        embedQueue.add({
+            channel_id : channel_id,
+            embed: this.generateEndPeriodEmbed()
+        },{
+            repeat:{
+                cron: cron_end,
                 tz: "Europe/Paris",
                 limit: 1
             }
@@ -82,7 +125,7 @@ module.exports = class PresenceSupervisor{
         return {
             "title": "Déclaration des présences",
             "description": description,
-            "url": "https://discordapp.com",
+            "url": "https://github.com/DevEkode/cesi_bot",
             "color": 10071592,
             "timestamp": date_now.toString(),
             "author": {
@@ -104,21 +147,44 @@ module.exports = class PresenceSupervisor{
     }
 
     generateStartPeriodEmbed(){
+        let end_time = moment(this.end).format('LT');
+        let description = moment().format('L');
         return {
             "title": "Fin déclaration des présences",
-            "description": "13/06/2020 - Matin",
-            "url": "https://discordapp.com",
+            "description": description,
+            "url": "https://github.com/DevEkode/cesi_bot",
             "color": 10071592,
-            "timestamp": "2020-06-13T10:05:38.205Z",
+            "timestamp": this.start.toString(),
             "author": {
                 "name": "CESI Bot",
-                "url": "https://discordapp.com",
+                "url": "https://github.com/DevEkode/cesi_bot",
                 "icon_url": "https://puu.sh/G2gn6/c26897ba03.png"
             },
             "fields": [
                 {
                     "name": "Les présences déclarées après ce message sont considéré avec du retard",
-                    "value": "Après 12h15 vous serez considérer comme absent"
+                    "value": "Après "+end_time+" vous serez considérer comme absent"
+                }
+            ]
+        };
+    }
+
+    generateEndPeriodEmbed(){
+        let text = moment(this.start).format('LT')+' - '+moment(this.end).format('LT');
+        return {
+            "title": "Fin de la période",
+            "url": "https://github.com/DevEkode/cesi_bot",
+            "color": 10071592,
+            "timestamp": this.end.toString(),
+            "author": {
+                "name": "CESI Bot",
+                "url": "https://github.com/DevEkode/cesi_bot",
+                "icon_url": "https://puu.sh/G2gn6/c26897ba03.png"
+            },
+            "fields": [
+                {
+                    "name": text,
+                    "value": "Les personnes n'ayant pas répondu sont déclarés comme absentes"
                 }
             ]
         };
