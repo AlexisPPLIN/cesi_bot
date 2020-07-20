23f8bb40dcd6d353da95ed1be7e1897ef4a66904
ekode <ekode@protonmail.com>
2020-07-20 22:23:45 +0200

    Added !viewperiodes base

---
+++ commands/viewPeriodes.js
@@ -0,0 +1,10 @@
+module.exports = {
+    name: "viewperiodes",
+    aliases: ['vp'],
+    description: "Donne la liste des périodes d'une date donnée",
+    args: true,
+    usage: "<date dd/mm/yyyy>",
+    execute(message, args) {
+        
+    }
+}
