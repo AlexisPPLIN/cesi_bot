2ee66a1e00a5b7db3c9c8cc11835bf96da03d193
ekode <ekode@protonmail.com>
2020-07-20 22:37:46 +0200

    Added basic argument validation

--- commands/viewPeriodes.js
+++ commands/viewPeriodes.js
@@ -1,3 +1,7 @@
+const PeriodesSupervisor = require('../classes/PeriodesSupervisor')
+
+const ArgumentValidationError = require('../Exceptions/ArgumentValidationError')
+
 module.exports = {
     name: "viewperiodes",
     aliases: ['vp'],
@@ -5,6 +9,13 @@ module.exports = {
     args: true,
     usage: "<date dd/mm/yyyy>",
     execute(message, args) {
-        
+        try{
+            let supervisor = new PeriodesSupervisor(args[0]);
+        }catch(e){
+            if(e instanceof ArgumentValidationError){
+                message.channel.send('Date argument not valid, check the format (DD/MM/YYYY) !')
+            }
+        }
+
     }
 }
