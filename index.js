58113bcc4a465be141e6ea832367550ef34213e0
AlexisPPLIN <alexis.poupelin@protonmail.com>
2020-07-21 21:30:28 +0200

    Merge fix

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ const env = require('./config.json');
 const client = new Discord.Client();
 const db = require('./models/index');
 
-const fs = require("fs")
-const vm = require('vm')
-embed_declaration_presence = require('.\\embed\\embed_declaration_presence.js');
-embed_fin_declaration_presence = require('.\\embed\\embed_fin_declaration_presence.js');
+const fs = require("fs");
+const vm = require('vm');
+const embed_declaration_presence = require('.\\embed\\embed_declaration_presence.js');
+const embed_fin_declaration_presence = require('.\\embed\\embed_fin_declaration_presence.js');
 
 const Queue = require('bull');
 let embedQueue = new Queue('embed', 'redis://127.0.0.1:6379');
@@ -22,35 +22,21 @@ for (const file of commandFiles) {
 }
 
 
-vm.runInThisContext(fs.readFileSync("./embed/embed_confirmation_presence_MP.js"))
-vm.runInThisContext(fs.readFileSync("./embed/embed_presence_jour.js"))
+//vm.runInThisContext(fs.readFileSync("./embed/embed_confirmation_presence_MP.js"))
+//vm.runInThisContext(fs.readFileSync("./embed/embed_presence_jour.js"))
 
-present = require('.\\commande\\present.js');
-liste_eleve = require('.\\commande\\liste_eleve.js');
-presences = require('.\\commande\\presences.js');
-link = require('.\\commande\\link.js');
-deleteEtudiant = require('.\\commande\\delete.js');
+const present = require('.\\commande\\present.js');
+const liste_eleve = require('.\\commande\\liste_eleve.js');
+const presences = require('.\\commande\\presences.js');
+const link = require('.\\commande\\link.js');
+const deleteEtudiant = require('.\\commande\\delete.js');
+
 const STATUT = {
 	RETARD: 1,
 	PRESENT: 2,
 	EN_ATTENTE: 3,
 	ABSENT: 4
 };
-
-//var fs = require("fs")
-//var vm = require('vm')
-//vm.runInThisContext(fs.readFileSync(".\\embed\\embed_confirmation_presence_MP.js"));
-//vm.runInThisContext(fs.readFileSync(".\\embed\\embed_presence_jour.js"));
-//vm.runInThisContext(fs.readFileSync(".\\embed\\embed_declaration_presence.js"));
-//vm.runInThisContext(fs.readFileSync(".\\embed\\embed_fin_declaration_presence.js"));
-//vm.runInThisContext(fs.readFileSync(".\\embed\\embed_liste_etudiant.js"));
-//vm.runInThisContext(fs.readFileSync(".\\commande\\present.js"));
-//vm.runInThisContext(fs.readFileSync(".\\commande\\liste_eleve.js"));
-//vm.runInThisContext(fs.readFileSync(".\\commande\\presences.js"));
-//vm.runInThisContext(fs.readFileSync(".\\commande\\link.js"));
-//vm.runInThisContext(fs.readFileSync(".\\commande\\delete.js"));
-
-
 
 client.once('ready', () => {
 	console.log('Ready!');
@@ -95,7 +81,7 @@ client.on('message', message => {
 		console.log(error);
 	}
 
-	var MessageMinuscule = message.content.toLowerCase()//afin de pas prendre en compte les majuscule ou minuscule 
+	let MessageMinuscule = message.content.toLowerCase()//afin de pas prendre en compte les majuscule ou minuscule
 
 	if (message.author.id !== client.user.id)//pour ignorer les msg du bot
 	{
@@ -115,16 +101,16 @@ client.on('message', message => {
 		}
 
 
-		if (MessageMinuscule.slice(0, 10) == '!présences' || MessageMinuscule.slice(0, 10) == '!presences'  ) {//si message commence par !présences
+		if (MessageMinuscule.slice(0, 10) === '!présences' || MessageMinuscule.slice(0, 10) === '!presences'  ) {//si message commence par !présences
 			presences(message, client ,MessageMinuscule,db,STATUT);
 		}
 
 
-		if (MessageMinuscule.slice(0, 6) == '!cours') {//si message commence par !cours
+		if (MessageMinuscule.slice(0, 6) === '!cours') {//si message commence par !cours
 		}
 
 
-		if (MessageMinuscule.slice(0, 5) == '!link') {//si message commence par !link
+		if (MessageMinuscule.slice(0, 5) === '!link') {//si message commence par !link
 			link(message, client ,MessageMinuscule,db,STATUT);
 		}
 
