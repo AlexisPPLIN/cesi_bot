165e1cf0a09eb6003cc9ef3510c4916a33f19724
ekode <ekode@protonmail.com>
2020-07-21 15:06:36 +0200

    Added every embed for !viewperiodes

--- commands/viewPeriodes.js
+++ commands/viewPeriodes.js
@@ -9,13 +9,18 @@ module.exports = {
     args: true,
     usage: "<date dd/mm/yyyy>",
     execute(message, args) {
+        let supervisor;
         try{
-            let supervisor = new PeriodesSupervisor(args[0]);
+            supervisor = new PeriodesSupervisor(args[0]);
         }catch(e){
             if(e instanceof ArgumentValidationError){
                 message.channel.send('Date argument not valid, check the format (DD/MM/YYYY) !')
             }
         }
+
+        supervisor.generateViewPeriodesEmbed(embed => {
+            message.channel.send({embed: embed});
+        });
 
     }
 }
