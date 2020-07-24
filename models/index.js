4520d7848a6182a13993741c51eaeae00c7b4d34
AlexisPPLIN <alexis.poupelin@protonmail.com>
2020-07-24 17:04:46 +0200

    Added start script and documentation

--- models/index.js
+++ models/index.js
@@ -1,12 +1,21 @@
 'use strict';
 
+const appRoot = require('app-root-path');
+const config_env = require(appRoot+'/config.json');
 const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
 const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
+
+
+if(!config_env.debug){
+  process.env.NODE_ENV = 'development';
+}else{
+  process.env.NODE_ENV = 'production';
+}
+let env = process.env.NODE_ENV || 'development';
 
 let sequelize;
 if (config.use_env_variable) {
